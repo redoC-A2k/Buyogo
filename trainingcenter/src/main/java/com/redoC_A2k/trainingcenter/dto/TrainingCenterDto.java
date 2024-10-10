0d75a1597286058e3bd5284ce588fc67d314e641
afshan ahmed khan <afshanahmeda2k@gmail.com>
2024-10-10 20:39:39 +0530

    Added validation on all fields

--- trainingcenter/src/main/java/com/redoC_A2k/trainingcenter/dto/TrainingCenterDto.java
+++ trainingcenter/src/main/java/com/redoC_A2k/trainingcenter/dto/TrainingCenterDto.java
@@ -3,11 +3,11 @@ package com.redoC_A2k.trainingcenter.dto;
 import java.util.List;
 
 import com.redoC_A2k.trainingcenter.entity.Address;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,12 +20,29 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class TrainingCenterDto {
+    @NotEmpty(message =  "Center name cannot be empty")
+    @Size(min = 1, max = 40, message = "Center name should not be less than 40 characters")
     private String centerName;
+    
+    @NotEmpty(message =  "Center code cannot be empty")
+    @Pattern(regexp = "[a-zA-Z0-9]{12}", message = "Center code should be alphanumeric and exactly 12 characters")
     private String centerCode;
+    
+    @Valid
     private Address centerAddress;
+
+    @Valid
     private int studentCapacity;
+
     private List<String> coursesOffered;
+    
+    @NotEmpty(message =  "Contact email cannot be empty")
+    @Email(message = "Invalid contact email")
     private String contactEmail;
+
+    @NotEmpty(message =  "Contact phone cannot be empty")
+    @Pattern(regexp = "^(0|91)?[7-9][0-9]{9}$", message = "Contact phone should be valid Indian phone number")
     private String contactPhone;
+
     private Long createdOn;
 }
--- trainingcenter/src/main/java/com/redoC_A2k/trainingcenter/exception/GlobalExceptionHandler.java
+++ trainingcenter/src/main/java/com/redoC_A2k/trainingcenter/exception/GlobalExceptionHandler.java
@@ -1,9 +1,17 @@
 package com.redoC_A2k.trainingcenter.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -12,17 +20,32 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.redoC_A2k.trainingcenter.dto.ErrorResponseDto;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
-    
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception ex, WebRequest webRequest) {
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(
-            webRequest.getDescription(false),
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            ex.getMessage(),
-            LocalDateTime.now()
-        );
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                LocalDateTime.now());
         // System.out.println("Global exception: " + ex);
         return new ResponseEntity<ErrorResponseDto>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        // return super.handleMethodArgumentNotValid(ex, headers, status, request);
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorsList = ex.getBindingResult().getAllErrors();
+        validationErrorsList.forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            validationErrors.put(fieldName, errorMessage);
+        });
+        System.out.println("Validation errors: " + validationErrors);
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
 }

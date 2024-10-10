ac8c2ab1a9cfcd6f9af014e94a8592d23262cf00
afshan ahmed khan <afshanahmeda2k@gmail.com>
2024-10-10 12:57:14 +0530

    Initial commit

---
+++ trainingcenter/src/test/java/com/redoC_A2k/trainingcenter/TrainingcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redoC_A2k.trainingcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
